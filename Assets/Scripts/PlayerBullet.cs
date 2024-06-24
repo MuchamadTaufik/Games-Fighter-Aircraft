37523b56f41b4f77e044adc222acfba5cb2a4a7a
MuchamadTaufik <muhamadtaufikm10@gmail.com>
2024-06-25 00:46:59 +0700

    Games Unity

---
+++ Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,19 @@
+﻿namespace Agate.SpaceShooter
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager Instance;
+        [SerializeField] private Enemy _enemyPrefab;
+        [SerializeField] private int _enemyCount;
+        [SerializeField] private float _enemySpawnRange;
+        [SerializeField] private Coin _CoinPrefab;
+        [SerializeField] private int _CointCount;
+        [SerializeField] private float _CoinSpawnRange;
+    }
+}
+
+private void Start()
+{
+    SpawnEnemies();
+    
+}
